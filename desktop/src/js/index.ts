3d969056baa0c87615b5dd586bf7c4bcb9e7f88c
guhongxin <1204738681@qq.com>
2020-07-27 13:30:00 +0800

    修改第三部分

--- desktop/src/js/index.ts
+++ desktop/src/js/index.ts
@@ -9,15 +9,36 @@ const recharge:HTMLElement = document.getElementById("recharge");
 const downApple:HTMLElement = document.getElementById("downApple");
 const downGoogle:HTMLElement = document.getElementById("downGoogle");
 const indicator:HTMLElement = document.querySelector(".indicator");
+const heroName:HTMLElement = document.querySelector(".hero-name");
+const heroGallery:HTMLElement = document.querySelector(".hero-gallery");
+
+const heroData = [
+  {
+    name: "东陵圣母",
+    introduce: "可以针对不同的媒体类型定义不同的样式"
+  },
+  {
+    name: "李白",
+    introduce: "可以针对不同的媒体类型定义不同的样式"
+  }
+]
+
 const pageIndex = 0; // 当前页
 function init() {
   // 初始化
+  removeClass(heroName, "hero-name-move");
   let swiper = new Swiper('.swiper-container', {
     direction: 'vertical',
     mousewheel: true,
     on: {
       slideChangeTransitionStart: function(){
+        //
         pagination(this.activeIndex);
+        if (this.activeIndex === 2) {
+          heroName.classList.add("hero-name-move")
+        } else {
+          removeClass(heroName, "hero-name-move");
+        }
       }
     }
   });
@@ -41,6 +62,20 @@ function init() {
       let index:number = Number(target.dataset.index);
       pagination(index);
       swiper.slideTo(index);
+    }
+  })
+  // 第三部分
+  heroGallery.addEventListener("click", function(e:any) {
+    let target:any  = e.target;
+    if (target.dataset.galleryImg === "img") {
+      removeClass(heroName, "hero-name-move");
+      let index:number = Number(target.dataset.galleryIndex);
+      (document.querySelector(".hero-name .hero-name-txt") as any).innerHTML = heroData[index]["name"];
+      (document.querySelector(".hero-name .introduce") as HTMLElement).innerHTML = heroData[index]["introduce"];
+      let timer = setTimeout(() => {
+        heroName.classList.add("hero-name-move");
+        clearTimeout(timer)
+      }, 50)
     }
   })
 }
