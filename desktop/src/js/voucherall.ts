2af751846acf79222c41dac61e2c4a2fbb3b3634
guhongxin <1204738681@qq.com>
2020-07-31 19:22:20 +0800

    选折渠道支付

--- desktop/src/js/voucherall.ts
+++ desktop/src/js/voucherall.ts
@@ -7,14 +7,25 @@ function init() {
   // 初始化
   let country:string = getQueryVariable("country"); // 获取选中的国家
   let result:string = Object.keys(ountryPaymentType[country]).reduce((total, key) => {
-    console.log("key", key);
     let str1:string = ""
     str1 = createPaymentType(key, ountryPaymentType[country][key]);
     total += str1
     return total
-  }, "")
-  conts.innerHTML = result
-  
+  }, "");
+  conts.innerHTML = result;
+}
+// 点击渠道绑定事件
+(window as any).channelItemClick = function() {
+  let dom = (window.event as any).path;
+  for (let i = 0; i < dom.length; i++) {
+    let tag = dom[i].dataset.tag
+    if (tag === "channel") {
+      let channelId = dom[i].dataset.channelid;
+      let channelName = dom[i].dataset.channelname;
+      location.href =  `dpurchase.html?paymentMethod=${channelName}&channelId=${channelId}`
+      return false
+    }
+  }
 }
 
 // 创建支付类型
@@ -37,8 +48,8 @@ function createPaymentChannelList(channel:Array<any>):string {
 }
 // 创建单个支付渠道
 function createPaymentChannelItem(channel):string {
-  let paymentTpe:string = `<li class="li" data-channelId=${channel.id}>
-    <a href="javascript:void(0)">
+  let paymentTpe:string = `<li class="li channelItem">
+    <a href="javascript:void(0)" onclick="channelItemClick()" data-tag="channel" data-channelid=${channel.id} data-channelname=${channel.name}>
       <img src="/static/img/Razer Pin.png" />
       ${channel.name}
       <div class="sub">
