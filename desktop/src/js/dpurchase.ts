f0e9f2da1d0ae0ab7b736adaf6a577c23be4e4fa
guhongxin <1204738681@qq.com>
2020-08-01 14:36:39 +0800

    请求

--- desktop/src/js/dpurchase.ts
+++ desktop/src/js/dpurchase.ts
@@ -2,17 +2,18 @@ import "normalize.css";
 import "../scss/dpurchase.scss";
 
 import { getQueryVariable } from "../utils/common";
+import Request from "../utils/request"
 
 // 全局变量
 const server: HTMLSelectElement = document.getElementById(
   "server"
 ) as HTMLSelectElement;
-const game: HTMLSelectElement = document.getElementById(
-  "game"
+const gameCurrency: HTMLSelectElement = document.getElementById(
+  "gameCurrency"
 ) as HTMLSelectElement;
-const characterID: HTMLInputElement = document.getElementById(
-  "characterID"
-) as HTMLInputElement;
+// const characterID: HTMLInputElement = document.getElementById(
+//   "characterID"
+// ) as HTMLInputElement;
 const characterName: HTMLSelectElement = document.getElementById(
   "characterName"
 ) as HTMLSelectElement;
@@ -23,27 +24,147 @@ const accept: HTMLInputElement = document.getElementById(
   "accept"
 ) as HTMLInputElement;
 const paymentMethodDom: HTMLElement = document.querySelector(".paymentMethod");
-
+const httpRequest = new Request("http://192.168.1.16:8080") // 请求
 function init(): void {
   // 初始化
   let paymentMethod: string = getQueryVariable("paymentMethod");
   paymentMethodDom.innerHTML = paymentMethod;
   restForm(); // 复位
   submit(); // 提交
+  getServerList().then((res:any) => {
+    createOptionDom(server, res, "id", "name");
+  }); // 服务器列表
+  server.addEventListener("change", function() {
+    // 监听服务器列表下拉change
+    amount.innerHTML = ""; // 复位道具
+    getCharacterList(this.value).then((res:any) => {
+      createOptionDom(characterName, res, "playerId", "intro")
+    })
+  })
+  // 币种 change 是否选中角色，如果选中请求充值道具未选中不请求
+  gameCurrency.addEventListener("change", function() {
+    let playerId:string = characterName.value;
+    if (playerId) {
+      let serverId:string = server.value;
+      let obj = {
+        serverId: serverId,
+        playerId: playerId,
+        currencyCode: this.value,
+        appId: 123
+      }
+      getitemsList(obj).then((res:any) => {
+        createAmountOptionDom(amount, res);
+      })
+    }
+  })
+  // 角色 change 是否选中币种，如果未选中提示，否则请求接口
+  characterName.addEventListener("change", function() {
+    let currencyCode = gameCurrency.value;
+    if (!currencyCode) {
+      alert("Please select Character Name!")
+      return false
+    }
+    let serverId:string = server.value;
+    let obj = {
+      serverId: serverId,
+      playerId: this.value,
+      currencyCode: currencyCode,
+      appId: 123
+    }
+    getitemsList(obj).then((res:any) => {
+      createAmountOptionDom(amount, res);
+    })
+  })
+}
+init();
+
+// 获取服务器列表
+function getServerList() {
+  return new Promise((resolve, reject) => {
+    httpRequest.getfetch("/servers?app_id=1").then((res:any) => {
+      if (res.code === 0) {
+        resolve(res.data)
+      } else {
+        alert(res.message)
+      }
+    }).catch(error => reject(error))
+  })
+}
+// 获取角色列表
+function getCharacterList(serverId:string) {
+  return new Promise((resolve, reject) => {
+    httpRequest.fetch({
+      url: "/characters",
+      method: "GET",
+      params: {
+        user_id: 123,
+        app_id: 1,
+        server_id: serverId
+      }
+    }).then((res:any) => {
+      if (res.code === 0) {
+        resolve(res.data)
+      } else {
+        alert(res.message)
+      }
+    }).catch(error => reject(error))
+  })
 }
 
-init();
+interface ItemRequestParam {
+  serverId:string | number;
+  playerId:string | number;
+  currencyCode:string |number,
+  appId: string |number
+}
+// 充值道具列表
+function getitemsList(param: ItemRequestParam) {
+  return new Promise((resolve, reject) => {
+    httpRequest.fetch({
+      url: "/items",
+      method: "GET",
+      params: {
+        app_id: param.appId,
+        server_id: param.serverId,
+        player_id: param.playerId,
+        currency_code: param.currencyCode
+      }
+    }).then((res:any) => {
+      if (res.code === 0) {
+        resolve(res.data)
+      } else {
+        alert(res.message)
+      }
+    }).catch(error => reject(error))
+  })
+}
+// 创建select option
+function createOptionDom(dom:HTMLElement, options:Array<any>, valKey, textkey) {
+  let result = options.reduce((total, itme) => {
+    total += `<option value ="${itme[valKey]}">${itme[textkey]}</option>`
+    return total
+  }, "<option style='display: none'></option>");
+  dom.innerHTML = result
+}
+
+// 创建amount option
+function createAmountOptionDom(dom:HTMLElement, options:Array<any>) {
+  let result = options.reduce((total, itme) => {
+    total += `<option value ="${itme.id}">${itme.name}-${itme.intro}</option>`
+    return total
+  }, "<option style='display: none'></option>");
+  dom.innerHTML = result
+}
 
 // 提交
 function submit() {
   let button: HTMLElement = document.querySelector(".button");
   button.onclick = () => {
-    // console.log("服务值:", server.value);
-    // console.log("游戏:", game.value);
-    // console.log("characterID:", characterID.value);
-    // console.log("characterName:", characterName.value);
-    // console.log("amount:", amount.value);
-    // console.log("accept:", accept.checked);
+    console.log("服务值:", server.value);
+    console.log("游戏:", gameCurrency.value);
+    console.log("characterName:", characterName.value);
+    console.log("amount:", amount.value);
+    console.log("accept:", accept.checked);
   };
 }
 // @ts-ignore
@@ -51,8 +172,7 @@ paypal.Buttons().render("#paypal-button-container");
 // 复位表单
 function restForm() {
   server.value = "";
-  game.value = "";
-  characterID.value = "";
+  gameCurrency.value = "";
   characterName.value = "";
   amount.value = "";
   accept.checked = false;
---
+++ desktop/src/utils/request.ts
@@ -0,0 +1,61 @@
+interface RequestParam {
+  url:string;
+  method: string;
+  params?: any;
+  data?: any;
+}
+
+class Request {
+  private baseURL:string;
+  constructor(url:string = "./") {
+    this.baseURL = url
+  }
+  getfetch(url:string, params?):any {
+    // get请求
+    let urlParam:string = "";
+    if (params) {
+      urlParam = this.parseParam(params)
+    }
+    let _url = this.baseURL + url + urlParam
+    return (window as any).fetch(_url).then(response => {
+      return  response.json()
+    })
+  }
+  postfetch(url:string, data?):any {
+    // post 请求
+    let _url = this.baseURL + url
+    return (window as any).fetch(_url, {
+      body: JSON.stringify(data),
+      method: "POST",
+      headers: {
+        'content-type': 'application/json'
+      }
+    }).then(response => {
+      return  response.json()
+    })
+  }
+  fetch(param:RequestParam):any {
+    let method= param.method.toUpperCase()
+    let result:any
+    switch(method) {
+      case "GET":
+        result = this.getfetch(param.url, param.params)
+        break
+      case "POST":
+        result = this.postfetch(param.url, param.data)
+        break
+    }
+    return result
+  }
+  parseParam(param:any) {
+    // js对象转成用&拼接的请求参数（转）
+    let result:Array<any> = Object.keys(param).reduce((total, key) => {
+      if (param[key]) {
+        total.push(key + "=" + param[key])
+      }
+      return total
+    }, [])
+    return "?" + result.join("&")
+  }
+}
+export default Request
